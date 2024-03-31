2823933bafdba2de6d9790ccf31ba109931ae1a7
paulsaitabau <paulsaitabau05@gmail.com>
2024-03-31 21:55:08 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+// Function to find the year a team won the Super Bowl from a given record
+ superbowlWin = (record) =>{
+    // Find the first record where the result is "W" (Win)
+    const winningRecord = record.find(record => record.result === "W");
+     
+    // Return the year of the winning record if found, otherwise return undefined
+    return winningRecord ? winningRecord.year : undefined;
+   }
+   
